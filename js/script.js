a0f36ecd2f630b7ef8b7a43d75e80e2cc9b8d132
Matheus João <70243348+sirjaao@users.noreply.github.com>
2024-09-05 21:17:43 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 "use strict";
 
 const apikey = "";
-const apiCountryURL = "https://flagcdn.com/w40/";//
+const apiCountryURL = "https://flagcdn.com/32x24/";
+const unsplashAccessKey = ""; // Insira sua chave de acesso do Unsplash
+const unsplashAPI = "https://api.unsplash.com/search/photos?query=";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
-// Corrija "docement" para "document"
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#Temperature span");
 const descElement = document.querySelector("#description");
@@ -17,7 +18,6 @@ const windElement = document.querySelector("#wind span");
 
 const weatherContainer = document.querySelector("#weather-data");
 
-// Funções
 const getWeatherData = async (city) => {
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}&lang=pt_br`;
 
@@ -27,8 +27,17 @@ const getWeatherData = async (city) => {
     return data;
 }
 
+const getCityImage = async (city) => {
+    const unsplashURL = `${unsplashAPI}${city}&client_id=${unsplashAccessKey}&orientation=landscape`;
+
+    const res = await fetch(unsplashURL);
+    const data = await res.json();
+
+    return data.results[0].urls.regular; // URL da primeira imagem encontrada
+}
+
 const showWeatherData = async (city) => {
-   const data = await getWeatherData(city); // Corrigido: apiWeartherURL não é uma função
+   const data = await getWeatherData(city);
 
    const countryCode = data.sys.country;
    const countryCodeLower = countryCode.toLowerCase();
@@ -44,10 +53,11 @@ const showWeatherData = async (city) => {
    humidityElement.innerText = `${data.main.humidity}%`;
    windElement.innerText = `${data.wind.speed}km/h`;
 
-   weatherContainer.classList.remove("hide");
-   
-   console.log("url:", countryElement)
+   // Adiciona a imagem de fundo
+   const cityImageUrl = await getCityImage(city);
+   document.body.style.backgroundImage = `url(${cityImageUrl})`;
 
+   weatherContainer.classList.remove("hide");
 }
 
 // Eventos
@@ -55,15 +65,12 @@ searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
     const city = cityInput.value;
-    console.log(city);
-    showWeatherData(city); // Mostra dados do tempo ao clicar
+    showWeatherData(city); 
 });
 
 cityInput.addEventListener("keyup", (e) => {
-
     if(e.code === "Enter") {
         const city = e.target.value;
-
         showWeatherData(city);
     }
-})
+});
